b73e0ce502076248520de9892f2d5fa649b625f0
latiao_home <latiaoqaq@gmail.com>
2024-05-22 18:34:14 +0800

    b

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,18 @@
 #include <WiFi.h>
 // #include <HTTPClient.h>
 // #include <ArduinoJson.h>
-// #include <DataProcess.h>
-// #include <FansControl.h>
+#include <DataProcess.h>
+#include <FansControl.h>
 // #include <U8g2lib.h>
 
 // U8G2_SSD1306_128X64_NONAME_F_HW_I2C oled(U8G2_R0,SDA,SCL,U8X8_PIN_NONE);
-// int RetCode = -1;
-// const char* ssd="WSGX";
-// const char* pasd="wsgx88888";
+ int RetCode = -1;
+const char* ssd="latiao";
+const char* pasd="guangqinfu332601";
 
-// const char * dst_addr = "192.168.2.6";
-// const int dst_port = 8000;
-// String recvdata = "";
+const char * dst_addr = "192.168.2.6";
+const int dst_port = 8000;
+String recvdata = "";
 
 
 
@@ -23,37 +23,16 @@
 // int disk_usage = -1;
 
 
-// DataProcess *DP = new DataProcess();
-// FansControl *FC = new FansControl(11,5000,0,8);
+DataProcess *DP = new DataProcess();
+//FansControl *FC = new FansControl(11,5000,0,8);
 
 
-// WiFiClient client;
-
-// hw_timer_t * tcp_timer = NULL;
+WiFiClient client;
 
 
-// void WriteData(String recvdata,int *Data_array[]);
 
-// void IRAM_ATTR TcpTimerEvent()
-// {
-//   if(client.connected())
-//   {
-    
-//   }
-//   else
-//   {
-//     client.stop();
-//     if(!client.connect(dst_addr,dst_port))
-//     {
-//       Serial.println("RecvFailed,Connecting.....");
-//     }
-//     else
-//     {
-//       Serial.println("ReConnected");
-//     }
-//     //timerAlarmDisable(tcp_timer);
-//   }
-// }
+
+
 
 // void setup() {
 
@@ -175,7 +154,11 @@
 lv_obj_t * cpu_arc;
 lv_obj_t * ram_arc;
 lv_obj_t * disk_arc;
-static void value_changed_event_cb(lv_event_t * e);
+
+static void cputemp_changed_event_cb(lv_event_t * e);
+static void ram_changed_event_cb(lv_event_t * e);
+static void Disk_changed_event_cb(lv_event_t * e);
+
 lv_obj_t * lv_example_arc_1(void);
 lv_obj_t * lv_example_arc_2(void);
 lv_obj_t * lv_example_arc_3(void);
@@ -241,18 +224,40 @@ void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data )
 
 void setup()
 {
-    String LVGL_Arduino = "Hello Arduino! ";
-    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
 
-    Serial.begin( 115200 );
-    Serial.println( LVGL_Arduino );
 
+  //Serial Init
+  Serial.begin(115200);
+
+  //Wifi Init
+  Serial.printf("开始连接WIFI");
+  Serial.printf("账号为：%d",ssd);
+  Serial.printf("密码为：%d",pasd);
+
+  WiFi.begin(ssd,pasd);
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("connected success!\n");
+
+  Serial.print("IP:");
+  Serial.print(WiFi.localIP());
+
+  
+//   client.connect(dst_addr,dst_port);
+//   #if 0
+//     pinMode(0,OUTPUT);
+//     ledcSetup(0, 10000, 10);
+//   //将 PWM 通道连接到引脚
+//     ledcAttachPin(0, 0);
+//     ledcWrite(0, 0);
+//     oled.enableUTF8Print();
+//   #endif
     lv_init();
-
-    /*Set a tick source so that LVGL will know how much time elapsed. */
     lv_tick_set_cb(millis);
 
-    /* register print function for debugging */
 #if LV_USE_LOG != 0
     lv_log_register_print_cb( my_print );
 #endif
@@ -310,18 +315,34 @@ void setup()
 
 void loop()
 {
-    if(Serial.available())
+  if(!client.connected())
+  {
+    client.stop();
+    client.connect(dst_addr,dst_port);
+    Serial.println("Connecting...");
+  }
+  else
+  {
+    client.write('1');
+    if(client.available()>0)
     {
-      String rcvdata = Serial.readStringUntil('\n');
-      int cpu_data = rcvdata.toInt();
-      Serial.println(cpu_data);
-      lv_arc_set_value(cpu_arc,cpu_data);
-      lv_obj_send_event(cpu_arc, LV_EVENT_VALUE_CHANGED,NULL);
-      lv_arc_set_value(disk_arc,cpu_data);
-      lv_obj_send_event(disk_arc, LV_EVENT_VALUE_CHANGED,NULL);
-      lv_arc_set_value(ram_arc,cpu_data);
-      lv_obj_send_event(ram_arc, LV_EVENT_VALUE_CHANGED,NULL);
+    recvdata = client.readStringUntil('\n');
+    Serial.println(recvdata);
+    DP->WriteData(recvdata);
+    DP->PrintData();
+    lv_arc_set_value(cpu_arc,DP->cpu_tmp);
+    lv_obj_send_event(cpu_arc, LV_EVENT_VALUE_CHANGED,NULL);
+    lv_arc_set_value(ram_arc,DP->ram_usage);
+    lv_obj_send_event(ram_arc, LV_EVENT_VALUE_CHANGED,NULL);
+    lv_arc_set_value(disk_arc,DP->disk_usage);
+    lv_obj_send_event(disk_arc, LV_EVENT_VALUE_CHANGED,NULL);
+    
+    //Serial.println(recvdata);
     }
+    
+    
+  }
+
     lv_task_handler(); /* let the GUI do its work */
     delay(5); /* let this time pass */
 }
@@ -342,6 +363,7 @@ lv_obj_t* lv_example_arc_1(void)
     lv_obj_set_style_pad_all(arc,3,LV_PART_MAIN);
     
     lv_obj_t * panel3 = lv_obj_create(arc);
+    lv_obj_set_style_border_width(panel3,0,LV_PART_MAIN);
     lv_obj_set_size(panel3,42,42);
     lv_obj_align_to(panel3,arc,LV_ALIGN_CENTER,0,0);
     lv_obj_set_style_radius(panel3,360,LV_PART_MAIN);
@@ -366,16 +388,15 @@ lv_obj_t* lv_example_arc_1(void)
     
     lv_obj_t * label = lv_label_create(panel3);
     lv_label_set_text_fmt(label,"%d",23);
-    //lv_obj_set_style_text_align(label,LV_TEXT_ALIGN_CENTER,LV_PART_MAIN);
-    //lv_obj_set_style_pad_all(label,0,LV_PART_MAIN);
-
     lv_obj_align(label,LV_ALIGN_CENTER,0,0);
-    lv_obj_set_style_bg_opa(label,0,LV_PART_MAIN);
-    //lv_obj_set_style_border_width(label,0,LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(label, LV_OPA_0, LV_PART_SCROLLBAR | LV_STATE_DEFAULT);
+    //lv_obj_set_style_border_width(label,4,LV_PART_MAIN);
     //lv_obj_set_style_border_color(label,0,LV_PART_MAIN);
-    //lv_style_set_text_font(label,&LV_FONT_MONTSERRAT_18,LV_PART_MAIN);
-    lv_obj_set_style_text_color(panel3,lv_color_make(255,255,255),LV_PART_MAIN);
-    lv_obj_add_event_cb(arc, value_changed_event_cb, LV_EVENT_VALUE_CHANGED, label);
+    //lv_style_set_text_font(label,&LV_FONT_MONTSERRAT_12,LV_PART_MAIN);
+    lv_obj_set_style_text_color(label,lv_color_make(255,255,255),LV_PART_MAIN);
+    lv_obj_set_scrollbar_mode(panel3,LV_SCROLLBAR_MODE_OFF);
+    lv_obj_set_scrollbar_mode(label,LV_SCROLLBAR_MODE_OFF);
+    lv_obj_add_event_cb(arc, cputemp_changed_event_cb, LV_EVENT_VALUE_CHANGED, label);
 
 
     lv_obj_remove_style(arc, NULL, LV_PART_KNOB);
@@ -383,6 +404,16 @@ lv_obj_t* lv_example_arc_1(void)
     lv_obj_set_style_arc_width(arc,5,LV_PART_INDICATOR);
     lv_obj_set_style_arc_color(arc, lv_color_hex(0x0f0f0f), LV_PART_MAIN);
     lv_obj_set_style_arc_rounded(arc, true, LV_PART_MAIN);
+
+
+    lv_obj_t * label2 = lv_label_create(lv_screen_active());
+    lv_label_set_text(label2,"CPU/°C");
+    lv_obj_align_to(label2, arc, LV_ALIGN_BOTTOM_MID, 0,20);
+    //lv_obj_set_style_border_width(label,4,LV_PART_MAIN);
+    //lv_obj_set_style_border_color(label,0,LV_PART_MAIN);
+    //lv_style_set_text_font(label,&LV_FONT_MONTSERRAT_12,LV_PART_MAIN);
+    lv_obj_set_style_text_color(label2,lv_color_make(255,255,255),LV_PART_MAIN);
+    lv_obj_set_scrollbar_mode(label2,LV_SCROLLBAR_MODE_OFF);
 
 
     //lv_obj_center(arc);
@@ -410,6 +441,7 @@ lv_obj_t * lv_example_arc_2(void)
     lv_obj_set_style_pad_all(arc,3,LV_PART_MAIN);
     
     lv_obj_t * panel3 = lv_obj_create(arc);
+    lv_obj_set_style_border_width(panel3,0,LV_PART_MAIN);
     lv_obj_set_size(panel3,42,42);
     lv_obj_align_to(panel3,arc,LV_ALIGN_CENTER,0,0);
     lv_obj_set_style_radius(panel3,360,LV_PART_MAIN);
@@ -436,11 +468,13 @@ lv_obj_t * lv_example_arc_2(void)
     lv_label_set_text_fmt(label,"%d",23);
     lv_obj_align(label,LV_ALIGN_CENTER,0,0);
     lv_obj_set_style_bg_opa(label,0,LV_PART_MAIN);
-    //lv_obj_set_style_border_width(label,0,LV_PART_MAIN);
+    //lv_obj_set_style_border_width(label,4,LV_PART_MAIN);
     //lv_obj_set_style_border_color(label,0,LV_PART_MAIN);
     //lv_style_set_text_font(label,&LV_FONT_MONTSERRAT_18,LV_PART_MAIN);
     lv_obj_set_style_text_color(panel3,lv_color_make(255,255,255),LV_PART_MAIN);
-    lv_obj_add_event_cb(arc, value_changed_event_cb, LV_EVENT_VALUE_CHANGED, label);
+    lv_obj_set_scrollbar_mode(panel3,LV_SCROLLBAR_MODE_OFF);
+    lv_obj_set_scrollbar_mode(label,LV_SCROLLBAR_MODE_OFF);
+    lv_obj_add_event_cb(arc, ram_changed_event_cb, LV_EVENT_VALUE_CHANGED, label);
 
 
     lv_obj_remove_style(arc, NULL, LV_PART_KNOB);
@@ -448,6 +482,15 @@ lv_obj_t * lv_example_arc_2(void)
     lv_obj_set_style_arc_width(arc,5,LV_PART_INDICATOR);
     lv_obj_set_style_arc_color(arc, lv_color_hex(0x0f0f0f), LV_PART_MAIN);
     lv_obj_set_style_arc_rounded(arc, true, LV_PART_MAIN);
+
+    lv_obj_t * label2 = lv_label_create(lv_screen_active());
+    lv_label_set_text(label2,"RAM/MB");
+    lv_obj_align_to(label2, arc, LV_ALIGN_BOTTOM_MID, 0,20);
+    //lv_obj_set_style_border_width(label,4,LV_PART_MAIN);
+    //lv_obj_set_style_border_color(label,0,LV_PART_MAIN);
+    //lv_style_set_text_font(label,&LV_FONT_MONTSERRAT_12,LV_PART_MAIN);
+    lv_obj_set_style_text_color(label2,lv_color_make(255,255,255),LV_PART_MAIN);
+    lv_obj_set_scrollbar_mode(label2,LV_SCROLLBAR_MODE_OFF);
 
 
     //lv_obj_center(arc);
@@ -473,18 +516,19 @@ lv_obj_t * lv_example_arc_3(void)
     lv_obj_set_style_pad_all(arc,3,LV_PART_MAIN);
     
     lv_obj_t * panel3 = lv_obj_create(arc);
-     lv_obj_set_size(panel3,42,42);
-     lv_obj_align(panel3,LV_ALIGN_CENTER,0,0);
-    // lv_obj_set_style_radius(panel3,360,LV_PART_MAIN);
-    // lv_obj_set_style_bg_color(panel3,lv_color_make(100,100,100),LV_PART_MAIN);
-    // lv_obj_set_style_bg_grad_color(panel3,lv_color_make(60,65,75),LV_PART_MAIN);
-    // lv_obj_set_style_bg_grad_dir(panel3,LV_GRAD_DIR_VER,LV_PART_MAIN);
-    // lv_obj_set_style_arc_width(panel3,0.2,LV_PART_MAIN);
-    // lv_obj_set_style_shadow_color(panel3,lv_color_make(5,10,15),LV_PART_MAIN);
-    // lv_obj_set_style_shadow_width(panel3,8,LV_PART_MAIN);
-    // lv_obj_set_style_shadow_spread(panel3,0,LV_PART_MAIN);
-    // lv_obj_set_style_shadow_offset_x(panel3,0,LV_PART_MAIN);
-    // lv_obj_set_style_shadow_offset_y(panel3,3,LV_PART_MAIN);
+    lv_obj_set_style_border_width(panel3,0,LV_PART_MAIN);
+    lv_obj_set_size(panel3,42,42);
+    lv_obj_align(panel3,LV_ALIGN_CENTER,0,0);
+    lv_obj_set_style_radius(panel3,360,LV_PART_MAIN);
+    lv_obj_set_style_bg_color(panel3,lv_color_make(100,100,100),LV_PART_MAIN);
+    lv_obj_set_style_bg_grad_color(panel3,lv_color_make(60,65,75),LV_PART_MAIN);
+    lv_obj_set_style_bg_grad_dir(panel3,LV_GRAD_DIR_VER,LV_PART_MAIN);
+    lv_obj_set_style_arc_width(panel3,0.2,LV_PART_MAIN);
+    lv_obj_set_style_shadow_color(panel3,lv_color_make(5,10,15),LV_PART_MAIN);
+    lv_obj_set_style_shadow_width(panel3,8,LV_PART_MAIN);
+    lv_obj_set_style_shadow_spread(panel3,0,LV_PART_MAIN);
+    lv_obj_set_style_shadow_offset_x(panel3,0,LV_PART_MAIN);
+    lv_obj_set_style_shadow_offset_y(panel3,3,LV_PART_MAIN);
     // lv_obj_t * panel_value = lv_obj_create(panel3);
     // lv_obj_set_size(panel_value,30,30);
     // lv_obj_align_to(panel_value,arc,LV_ALIGN_CENTER,0,0);
@@ -499,11 +543,13 @@ lv_obj_t * lv_example_arc_3(void)
     lv_label_set_text_fmt(label,"%d",23);
     lv_obj_align(label,LV_ALIGN_CENTER,0,0);
     lv_obj_set_style_bg_opa(label,0,LV_PART_MAIN);
-    //lv_obj_set_style_border_width(label,0,LV_PART_MAIN);
+    //lv_obj_set_style_border_width(label,4,LV_PART_MAIN);
     //lv_obj_set_style_border_color(label,0,LV_PART_MAIN);
     //lv_style_set_text_font(label,&LV_FONT_MONTSERRAT_18,LV_PART_MAIN);
     lv_obj_set_style_text_color(panel3,lv_color_make(255,255,255),LV_PART_MAIN);
-    lv_obj_add_event_cb(arc, value_changed_event_cb, LV_EVENT_VALUE_CHANGED, label);
+    lv_obj_set_scrollbar_mode(panel3,LV_SCROLLBAR_MODE_OFF);
+    lv_obj_set_scrollbar_mode(label,LV_SCROLLBAR_MODE_OFF);
+    lv_obj_add_event_cb(arc, Disk_changed_event_cb, LV_EVENT_VALUE_CHANGED, label);
 
 
     lv_obj_remove_style(arc, NULL, LV_PART_KNOB);
@@ -511,6 +557,15 @@ lv_obj_t * lv_example_arc_3(void)
     lv_obj_set_style_arc_width(arc,5,LV_PART_INDICATOR);
     lv_obj_set_style_arc_color(arc, lv_color_hex(0x0f0f0f), LV_PART_MAIN);
     lv_obj_set_style_arc_rounded(arc, true, LV_PART_MAIN);
+    lv_obj_t * label2 = lv_label_create(lv_screen_active());
+    lv_label_set_text(label2,"Disk/%");
+    lv_obj_align_to(label2, arc, LV_ALIGN_BOTTOM_MID, 0,20);
+    //lv_obj_set_style_border_width(label,4,LV_PART_MAIN);
+    //lv_obj_set_style_border_color(label,0,LV_PART_MAIN);
+    //lv_style_set_text_font(label,&LV_FONT_MONTSERRAT_12,LV_PART_MAIN);
+    lv_obj_set_style_text_color(label2,lv_color_make(255,255,255),LV_PART_MAIN);
+    lv_obj_set_scrollbar_mode(label2,LV_SCROLLBAR_MODE_OFF);
+
 
 
     //lv_obj_center(arc);
@@ -521,7 +576,16 @@ lv_obj_t * lv_example_arc_3(void)
     return arc;
 
 }
-static void value_changed_event_cb(lv_event_t * e)
+static void cputemp_changed_event_cb(lv_event_t * e)
+{
+    lv_obj_t * arc = lv_event_get_target_obj(e);
+    lv_obj_t * label =(lv_obj_t*)lv_event_get_user_data(e);
+    int32_t value = lv_arc_get_value(arc);
+    //Serial.printf("value%llu\n",value);
+
+    lv_label_set_text_fmt(label,"%d°",value);
+}
+static void ram_changed_event_cb(lv_event_t * e)
 {
     lv_obj_t * arc = lv_event_get_target_obj(e);
     lv_obj_t * label =(lv_obj_t*)lv_event_get_user_data(e);
@@ -529,7 +593,14 @@ static void value_changed_event_cb(lv_event_t * e)
     //Serial.printf("value%llu\n",value);
 
     lv_label_set_text_fmt(label,"%d",value);
-    LV_LOG_USER(value);
 }
 
+static void Disk_changed_event_cb(lv_event_t * e)
+{
+    lv_obj_t * arc = lv_event_get_target_obj(e);
+    lv_obj_t * label =(lv_obj_t*)lv_event_get_user_data(e);
+    int32_t value = lv_arc_get_value(arc);
+    //Serial.printf("value%llu\n",value);
 
+    lv_label_set_text_fmt(label,"%d%%",value);
+}
